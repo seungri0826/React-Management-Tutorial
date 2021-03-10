2fb044c5116e20b4b044c304c263501ae5174f60
seungri0826 <seungri0826@naver.com>
2021-03-11 01:12:15 +0900

    Create Customer Component (2)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,61 @@
 import './App.css';
 import Customer from './components/Customer';
 
-const customer = {
-  'name': '유승리',
-  'birthday': '990826',
-  'gender': '여자',
-  'job': '대학생'
-}
+const customers = [
+    {
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/1',
+    'name': '유승리',
+    'birthday': '990826',
+    'gender': '여자',
+    'job': '대학생'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '유승은',
+    'birthday': '040827',
+    'gender': '여자',
+    'job': '고등학생'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': '류경주',
+    'birthday': '721016',
+    'gender': '여자',
+    'job': '회사원'
+  }
+  ]
+
 function App() {
   return (
-    <Customer
-      name={customer.name}
-      birthday={customer.birthday}
-      gender={customer.gender}
-      job={customer.job}
-    />
+      <div>
+        <Customer
+            id={customers[0].id}
+            image={customers[0].image}
+            name={customers[0].name}
+            birthday={customers[0].birthday}
+            gender={customers[0].gender}
+            job={customers[0].job}
+        />
+        <Customer
+            id={customers[1].id}
+            image={customers[1].image}
+            name={customers[1].name}
+            birthday={customers[1].birthday}
+            gender={customers[1].gender}
+            job={customers[1].job}
+        />
+        <Customer
+            id={customers[2].id}
+            image={customers[2].image}
+            name={customers[2].name}
+            birthday={customers[2].birthday}
+            gender={customers[2].gender}
+            job={customers[2].job}
+        />
+      </div>
   );
 }
 
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -16,12 +16,29 @@ class Customer extends React.Component {
 function Customer(props) {
     return (
         <div>
-            <h2>{props.name}</h2>
+            <CustomerProfile id={props.id} image={props.image} name={props.name}/>
+            <CustomerInfo birthday={props.birthday} gender={props.gender} job={props.job}/>
+        </div>
+    );
+}
+
+function CustomerProfile(props) {
+    return (
+        <div>
+            <img src={props.image} alt={"profile"}/>
+            <h2>{props.name}({props.id})</h2>
+        </div>
+    )
+}
+
+function CustomerInfo(props) {
+    return (
+        <div>
             <p>{props.birthday}</p>
             <p>{props.gender}</p>
             <p>{props.job}</p>
         </div>
-    );
+    )
 }
 
 export default Customer;
